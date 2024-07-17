784057a104445d10d52bb5085178ff3c15c64b5a
Ruben <amon007>
2024-07-17 16:04:12 +0300

    1

--- src/components/PostBlock.jsx
+++ src/components/PostBlock.jsx
@@ -5,32 +5,46 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 function PostBlock() {
     
     return(
-        <div className='w-11/12 h-[99px] flex items-center second_bg_color rounded-xl my-4 box-shadow'>
-            <div className=' flex w-full px-2'>
-                <img src="/images/avatar.png" alt="" className=' w-14 h-14'/>
+        <div className='w-11/12 lg:h-[99px] max-lg:flex-col max-sm:flex-col  flex items-center second_bg_color rounded-xl my-4 box-shadow'>
+           
+            <div className=' flex w-full p-2 justify-between'>
+               <div className=' flex'>
+                <img src="/images/avatar.png" alt="" className='w-14 h-14'/>
                 <div>
                     <h4 className=' font-semibold text-base'>Amon007</h4>
-                    <p className=' font-semibold text-xs brown_color'>6h ago</p>
+                    <p className='flex items-center font-semibold text-xs brown_color'>6h ago  <div className='max-sm:mx-1 light-first-bg-color p-1 rounded-md second_color text-xs'>Programming</div></p>
+                    
                 </div>
+                
+               </div>
+
+            </div>
+            <div className=' px-2 py-1 max-lg:w-full'>
+                <h3 className=' max-sm:text-xl font-semibold lg:text-2xl text-sm'>Hello World Bla bla bal bal balblalb</h3>
             </div>
 
-            <div className='w-full px-6'>
-                <div>
-                    <h3 className=' font-semibold text-2xl'>Hello World</h3>
-                </div>
-                <div className=' flex'>
-                    <div className='light-first-bg-color p-1 rounded-md second_color'>Programming</div>
+            <div className='w-full p-2'>
+               
+                <div className=' flex max-md:justify-between'>
+                    
                     <div className='flex items-center px-2'>
                         <FontAwesomeIcon icon={faThumbsUp} className='primary-first-color h-[25px] w-[25px] px-1'/>
                         <p>10</p>
                     </div>
-                    <div className='flex items-center px-2'>
+                    {/* <div className='flex items-center px-2'>
                         <FontAwesomeIcon icon={faEnvelope} className='primary-third-color h-[25px] w-[25px] px-1'/>
                         <p>10</p>
+                    </div> */}
+                    <div className=' px-2 flex justify-around items-center rounded-xl neutral-first-bg-color'>
+                        <FontAwesomeIcon icon={faEnvelope} className='h-[25px] w-[25px] pr-2 fourth_color'/>
+                        <p className=' fourth_color'>Պատասխանել</p>
+                        <div className='mx-1 primary-first-bg-color w-5 h-6 flex justify-center items-center rounded-r-md'>
+                            10
+                        </div>
                     </div>
                 </div>
             </div>
-
+{/* 
             <div className='w-full px-2'>
                 <div className=' w-52 h-8 flex justify-around items-center rounded-xl neutral-first-bg-color'>
                     <FontAwesomeIcon icon={faEnvelope} className='h-[25px] w-[25px] pr-2 fourth_color'/>
@@ -40,7 +54,7 @@ function PostBlock() {
                     </div>
                 </div>
             </div>
-            
+             */}
         </div>
     )
 }
