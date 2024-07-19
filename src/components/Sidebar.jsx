c7d9ca01b861e5c94bd7446e8770ade303f31dee
Ruben <amon007>
2024-07-19 15:08:37 +0300

    planshet

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import CreateTheam from './MenuComponents/CreateTheam.jsx'
 import Category from './MenuComponents/Category.jsx'
 import Leaders from './MenuComponents/Leaders.jsx'
+import Login from './MenuComponents/Login.jsx'
+import ListUsers from './MenuComponents/ListUsers.jsx'
 function Sidebar() {
     return (
         <div className='max-md:hidden h-full flex flex-col justify-center items-center border-r border-[#999999]'>
             <CreateTheam/>
+
+            <div className=' max-lg:block hidden'>
+                <Login />
+            </div>
+
             <Category />
+
             <Leaders />
+
+            <div className=' max-lg:block hidden'>
+                <ListUsers />
+            </div>
+            
         </div>
     )
 }
